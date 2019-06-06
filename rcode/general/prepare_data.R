b38939c9f91fbd55449a54e743252191f00354ca
Jan-Philipp Kolb <JanPhilipp.Kolb@gmail.com>
2019-06-06 22:12:58 +0200

    einfache Grafiken

--- rcode/general/prepare_data.R
+++ rcode/general/prepare_data.R
@@ -11,6 +11,7 @@ data_path <- paste0(git_path,"data/")
 wahldat <- read.csv2("http://offenedaten.frankfurt.de/dataset/806813a2-8884-46ee-b1c9-4d4f9ff4eee9/resource/98812021-fc2a-446b-88c0-67e5180af948/download/zprojekteopen-datadatenamt-12landtagswahl-2018ergebnisseergebnisselw2018landesstimmen_stadtteile.csv")
 
 head(wahldat)
+View(wahldat)
 
 write.csv2(wahldat,file=paste0(data_path,"wahldat_ffm.csv"))
 
@@ -22,15 +23,20 @@ write.csv2(wahldat,file=paste0(data_path,"wahldat_ffm.csv"))
 
 
 data.path <- "J:/Work/GESISPanel/04_persoenliche_ordner/kolb/Workshops/rintro19_ffm/data/"
-
+git_path <- "D:/github/ffm_rintro/"
+data_path <- paste0(git_path,"data/")
 
 # datb <- readxl::read_xls(paste0(data.path,"bauenwohnen2011.xls"))
 datb <- read.csv2(paste0(data.path,"bauenwohnen2011.csv"))
-
+datb <- read.csv2(paste0(data_path,"bauenwohnen.csv"))
+  
 colnames(datb)
 
-dat <- datb[,"Stadtteil"]
+datb <- datb[-which(datb$Stadtteil=="Frankfurt am Main"),]
+datb <- data.frame(datb)
+dat <- data.frame(Stadtteil=datb[,2])
 
+dat$bautaet <- datb[,125]
 dat$Spielplaetze_100k <- datb[,16]
 
 dat$Spiel100K <- cut(datb[,16],3)
@@ -41,16 +47,55 @@ levels(dat$baugenehm12) <- c("sehr wenig","wenig","mittel","viele","sehr viele")
 
 dat$wohnungsbestand <- datb[,18]
 
+
+dat$wohnraumversorgung_k <- datb$Wohnraumversorgung.Anzahl.der.Einwohnerinnen.und.Einwohner.je.Wohnung.2011
 ################
 # Daten anschauen
 
 colnames(datb)[16]
 colnames(datb)[78]
 
+erg <- vector()
+
+for (i in 1:ncol(datb)){
+  erg[i] <- sum(is.na(datb[,i]))
+}
+
+# Clusteranalyse
+nan <- which(erg==0)
+nan <- nan[-1]
+clust <- kmeans(datb[,c(9,10,11,12,13,14:25)],4)
+
+dat$clust <- clust$cluster
+
+
+
 ################
 # Daten speichern
 
+dat <- data.frame(dat)
 
 write.csv2(dat,file=paste0(git_path,"/data/bauenwohnen_teil.csv"))
 
 save(dat,file=paste0(git_path,"/data/bauenwohnen_teil.RData"))
+
+
+#####################
+# XML 
+
+
+# install.packages("flatxml")
+
+
+
+# devtools_install.packages("xmltools")
+# install.packages("XML")
+
+#library("xmltools")
+
+# https://stackoverflow.com/questions/33446888/r-convert-xml-data-to-data-frame
+
+dat <- xmlParse("http://offenedaten.frankfurt.de/dataset/912fe0ab-8976-4837-b591-57dbf163d6e5/resource/48378186-5732-41f3-9823-9d1938f2695e/download/parkdaten_dyn.xml")
+
+
+# https://r4ds.had.co.nz/data-import.html
